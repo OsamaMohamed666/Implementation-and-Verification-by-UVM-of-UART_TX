69088396ea842737cd561784ad56a18aec810aec
Osama Mohamed Elsheikh <osama.mohamed.elshiekh@gmail.com>
2025-04-18 16:58:32 +0200

    Update uart_tx_fsm.v

--- DESIGN/uart_tx_fsm.v
+++ DESIGN/uart_tx_fsm.v
@@ -1,137 +1,90 @@
+module FSM (
 
-module uart_tx_fsm  (
+input            clk, 
+input            rst,
+input            data_valid,
+input            ser_done,
+input            par_en,
+output reg [1:0] mux_sel,
+output reg       busy,
+output reg       ser_en
 
- input   wire                  CLK,
- input   wire                  RST,
- input   wire                  Data_Valid, 
- input   wire                  ser_done, 
- input   wire                  parity_enable, 
- output  reg                   Ser_enable,
- output  reg     [1:0]         mux_sel, 
- output  reg                   busy
 );
 
+localparam   idle  = 3'b000,
+             start  = 3'b001,
+             serial = 3'b010,
+			 parity = 3'b011,
+			 stop   = 3'b100;
+			 
+reg [2:0]  current_state , next_state;
 
-// gray state encoding
-parameter   [2:0]      IDLE   = 3'b000,
-                       start  = 3'b001,
-					   data   = 3'b011,
-					   parity = 3'b010,
-					   stop   = 3'b110 ;
+reg busy_tmp;
 
-reg         [2:0]      current_state , next_state ;
+always @(posedge clk or negedge rst)
+  begin
+    if(!rst)
+      begin
+        current_state<=idle;
+		busy <= busy_tmp;
+      end
+    else 
+      begin 
+	   busy <= busy_tmp;
+	   current_state<=next_state;
+      end
+   end
+
+
+//NEXT STATE LOGIC & OUTPUT LOGIC
+always@(*)
+  begin
+    ser_en = 1'b0;
+	mux_sel = 2'b00;
+	busy_tmp = 0;
+    case(current_state)
+    idle : begin
+			mux_sel=2'b11;
+			if (data_valid)
+			 next_state = start;
+			else 
+			 next_state = current_state;
+    	   end
+
+    start : begin
+			 busy_tmp=1'b1;
+			 next_state= serial;
+			end
+       
+	serial :begin
+			 ser_en =1'b1;
+	         mux_sel=2'b01;
+             busy_tmp=1'b1;
+			 if(ser_done) 
+			  begin
+			   ser_en=1'b0;
+			   if(par_en)
+				next_state=parity;
+			   else 
+			    next_state = stop;
+			  end 
+			 else
+			  next_state=current_state;
+			end
+                
+	parity :begin
+            mux_sel=2'b10;
+            busy_tmp=1'b1;
+			next_state=stop;
+			end
+	stop :  begin
+	        mux_sel=2'b11;
+            busy_tmp=1'b1;
+			next_state=idle;
+			end
+	default : next_state=idle;	
+	endcase
+  end
 			
-
-reg                    busy_c ;
-			
-//state transiton 
-always @ (posedge CLK or negedge RST)
- begin
-  if(!RST)
-   begin
-    current_state <= IDLE ;
-   end
-  else
-   begin
-    current_state <= next_state ;
-   end
- end
- 
-
-// next state logic
-always @ (*)
- begin
-  case(current_state)
-  IDLE   : begin
-            if(Data_Valid)
-			 next_state = start ;
-			else
-			 next_state = IDLE ; 			
-           end
-  start  : begin
-			 next_state = data ;  
-           end
-  data   : begin
-            if(ser_done)
-			 begin
-			  if(parity_enable)
-			   next_state = parity ;
-              else
-			   next_state = stop ;			  
-			 end
-			else
-			 next_state = data ; 			
-           end
-  parity : begin
-			 next_state = stop ; 
-           end
-  stop   : begin
-			 next_state = IDLE ; 
-           end	
-  default: begin
-			 next_state = IDLE ; 
-           end	
-  endcase                 	   
- end 
-
- 
-// output logic
-always @ (*)
- begin
-     Ser_enable = 1'b0 ;
-     mux_sel = 2'b00 ;	
-     busy_c = 1'b0 ;
-  case(current_state)
-  IDLE   : begin
-			Ser_enable = 1'b0 ;
-            mux_sel = 2'b11 ;	
-            busy_c = 1'b0 ;									
-           end
-  start  : begin
-			Ser_enable = 1'b0 ;  
-            busy_c = 1'b1 ;
-            mux_sel = 2'b00 ;	
-           end
-  data   : begin 
-			Ser_enable = 1'b1 ;    
-            busy_c = 1'b1 ;
-            mux_sel = 2'b01 ;	
-            if(ser_done)
-			 Ser_enable = 1'b0 ;  
-			else
- 			 Ser_enable = 1'b1 ;              			 
-           end
-  parity : begin
-            busy_c = 1'b1 ;
-            mux_sel = 2'b10 ;	
-           end
-  stop   : begin
-            busy_c = 1'b1 ;
-            mux_sel = 2'b11 ;			
-           end		
-  default: begin
-            busy_c = 1'b0 ;
-			Ser_enable = 1'b0 ;
-            mux_sel = 2'b00 ;		
-           end	
-  endcase         	              
- end 
- 
- 
-
-//register output 
-always @ (posedge CLK or negedge RST)
- begin
-  if(!RST)
-   begin
-    busy <= 1'b0 ;
-   end
-  else
-   begin
-    busy <= busy_c ;
-   end
- end
-  
-
+          	
 endmodule
- 
